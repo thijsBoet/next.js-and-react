1e6c58166c445d6d017c110ef15b5afdb9de23a7
Matthijs Boet <m.boet@chello.nl>
2022-04-03 20:58:47 +0200

    finished first project

--- 4-file-based-routing-project/pages/events/[...slug].js
+++ 4-file-based-routing-project/pages/events/[...slug].js
@@ -1,9 +1,69 @@
+import { useRouter } from 'next/router';
+import { getFilteredEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
+import ResultsTitle from '../../components/events/results-title';
+import Button from '../../components/ui/button';
+import ErrorAlert from '../../components/ui/error-alert';
+
 const FilteredEventsPage = () => {
+	const router = useRouter();
+
+	const filterData = router.query.slug;
+	if (!filterData) {
+		return <p className='center'>Loading...</p>;
+	}
+
+	const filteredYear = filterData[0];
+	const filteredMonth = filterData[1];
+
+	const year = +filteredYear;
+	const month = +filteredMonth;
+
+	if (
+		isNaN(year) ||
+		isNaN(month) ||
+		year < 2021 ||
+		year > 2030 ||
+		month < 1 ||
+		month > 12
+	) {
+		return (
+			<>
+				<ErrorAlert>
+					<p>Invalid Filter Data. Please adjust your values!</p>
+				</ErrorAlert>
+				<div className='center'>
+					<Button link='/events'>Show All Events</Button>
+				</div>
+			</>
+		);
+	}
+
+	const filteredEvents = getFilteredEvents({ year, month });
+
+	if (!filteredEvents || filteredEvents.length === 0) {
+		return (
+			<>
+				<ErrorAlert>
+					<p>No Events found for chosen filter data!</p>
+				</ErrorAlert>
+				<div className='center'>
+					<Button className='center' link='/events'>
+						Show All Events
+					</Button>
+				</div>
+			</>
+		);
+	}
+
+	const date = new Date(year, month - 1);
+
 	return (
-		<div>
-			<h1>Filtered Events</h1>
-		</div>
+		<>
+			<ResultsTitle date={date} />
+			<EventList items={filteredEvents} />
+		</>
 	);
-}
+};
 
 export default FilteredEventsPage;
--- 4-file-based-routing-project/pages/events/[eventId].js
+++ 4-file-based-routing-project/pages/events/[eventId].js
@@ -3,17 +3,29 @@ import { getEventById } from '../../dummy-data';
 import EventContent from '../../components/event-detail/event-content';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventSummary from '../../components/event-detail/event-summary';
+import ErrorAlert from '../../components/ui/error-alert';
+import Button from '../../components/ui/button';
 
 const EventDetail = () => {
 	const router = useRouter();
 
 	const eventId = router.query.eventId;
 	const event = getEventById(eventId);
-	const { title, date, location, image, description } = event;
 
-	if (!eventId) {
-		return <p>No event found!</p>;
+	if (!event) {
+		return (
+			<>
+				<ErrorAlert>
+					<p>No event found!</p>
+				</ErrorAlert>
+				<div className='center'>
+					<Button link='/'>Back</Button>
+				</div>
+			</>
+		);
 	}
+
+	const { title, date, location, image, description } = event;
 
 	return (
 		<>
